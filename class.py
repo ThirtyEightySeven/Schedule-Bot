4ed355236dab55b867cbe59c0b0db399399dd8f6
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-11-21 18:28:37 -0500

    Add class stubs

---
+++ class.py
@@ -0,0 +1,6 @@
+class Class:
+    __slots__ = ['name', 'time', 'location']
+    def __init__(self, name, time, location):
+        self.name = name
+        self.time = time
+        self.location = location
