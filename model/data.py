c63f716919d4ce27a822114cda78b2d533af167f
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-11-22 13:30:35 -0500

    Replace JSON model with Pickle

--- model/data.py
+++ model/data.py
@@ -1,18 +1,21 @@
 from model.user import User
-
-import json
+import pickle
 
 class Data:
     __slots__ = ['db']
 
-    def __init__(self, db_file='db.json'):
-        with open('db.json', 'r') as read_file:
-            self.db = json.load(read_file)
+    def __init__(self, db_file='db.data'):
+        self.read_data(db_file)
 
     def add_user(self, user):
         self.db['users'][user.name] = user
         print(self.db)
 
-    def write_data(self, db_file='db.json'):
-        with open("db.json", "w") as write_file:
-            json.dump(self.db, write_file)
+    
+    def read_data(self, db_file='db.data'):
+        self.db = pickle.load(open(db_file, 'rb'))
+
+    def write_data(self, db_file='db.data'):
+        fw = open(db_file, 'wb')
+        pickle.dump(self.db, fw)
+        fw.close()
